b7d49f732fbe632011fb4095ddbf55bdab644765
anil843324 <anilkrsingh14200@gmail.com>
2022-09-12 16:56:21 +0530

    i have tryed my best  to build within 5 hours

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Home from './pages/Home';
 function App() {
   return (
     <>
-       {/* <Navbar/> */}
+       <Navbar/>
        <Home/>
     </>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,18 +19,18 @@ const Navbar = () => {
 
 
   return (
-    <div className="w-full  h-[60px] bg-black">
+    <div className="w-full  h-[60px] bg-slate-400">
       <div className="max-w-[1240px] mx-auto px-4 flex justify-between items-center h-full">
         <div>
-          <h1 className="text-[#00d8ff] ">DEFI</h1>
+   
+             <img src="https://coincap.io/static/logos/black.svg"  alt="logo"  className="w-16 h-16 cursor-pointer" />
         </div>
         <div className=" hidden md:flex">
-          <ul className="flex text-white items-center">
-            <li>Platform</li>
-            <li>Developers</li>
-            <li>Community</li>
-            <li>About</li>
-            <button className="ml-4">Use Defi</button>
+          <ul className="flex  items-center cursor-pointer">
+          <li>Coins</li>
+            <li>Exchanges</li>
+            <li>Swap</li>
+           
           </ul>
         </div>
         {/* Ham burger */}
@@ -38,18 +38,18 @@ const Navbar = () => {
 
             {/* <AiOutlineMenu size={30} className="text-white" /> */}
             {
-                toggle ?  <AiOutlineMenu size={30} className="text-white cursor-pointer" onClick={()=> handleToggle()} /> : <AiOutlineClose size={30} className="text-white cursor-pointer"  onClick={()=> handleToggle()} />
+                toggle ?  <AiOutlineMenu size={30} className=" cursor-pointer" onClick={()=> handleToggle()} /> : <AiOutlineClose size={30} className="text-white cursor-pointer"  onClick={()=> handleToggle()} />
             }
         </div>
         {/* mobile menu */}
         {
-            !toggle ? <div className="  md:hidden w-full  bg-black text-white absolute top-[60px] left-0  flex justify-center text-center">
-          <ul className="text-2xl">
-            <li>Platform</li>
-            <li>Developers</li>
-            <li>Community</li>
-            <li>About</li>
-            <button className="m-8 ">Use Defi</button>
+            !toggle ? <div className="  md:hidden w-full  bg-black  absolute top-[60px] left-0  flex justify-center text-center">
+          <ul className="text-2xl cursor-pointer ">
+            <li>Coins</li>
+            <li>Exchanges</li>
+            <li>Swap</li>
+           
+          
           </ul>
         </div> :<></>
         }
