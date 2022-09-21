eb9cb1376c9dc9404bcf32539e3681481d0842e6
anil843324 <anilkrsingh14200@gmail.com>
2022-09-21 19:03:50 +0530

    footer bug fixed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-import {BsTwitter} from "react-icons/bs"
-import {AiFillFacebook} from "react-icons/ai"
+import { BsTwitter } from "react-icons/bs";
+import { AiFillFacebook } from "react-icons/ai";
 const Footer = () => {
   return (
     <div className=" ">
@@ -11,51 +11,71 @@ const Footer = () => {
           <h2>COINCAPI.IO</h2>
           {/* child div */}
           <div className=" flex flex-col text-white     cursor-pointer    ">
-            <span className=" opacity-50 hover:opacity-100" >Methodology</span>
+            <span className=" opacity-50 hover:opacity-100">Methodology</span>
             <span className="opacity-50 hover:opacity-100e">Support</span>
             <span className="opacity-50 hover:opacity-100">Our API</span>
-            <span className="opacity-50 hover:opacity-100">Rate Compariosn</span>
+            <span className="opacity-50 hover:opacity-100">
+              Rate Compariosn
+            </span>
             <span className="opacity-50 hover:opacity-100">Careers</span>
           </div>
         </div>
 
         {/* legals && discalmer */}
 
-         <div className=" flex ">
-        <div>
-          {/* Inner Div */}
-          <div className=" flex flex-col gap-2 ">
-            {/* inner child 1 */}
-            <div>
-              <h2>LEGALS</h2>
-              {/* child div */}
-              <div className="flex flex-col text-white    cursor-pointer">
-                <span className=" opacity-50 hover:opacity-100">Terms of Service</span>
-                <span className=" opacity-50 hover:opacity-100">Privacy Policy</span>
+        <div className=" flex ">
+          <div>
+            {/* Inner Div */}
+            <div className=" flex flex-col gap-2 ">
+              {/* inner child 1 */}
+              <div>
+                <h2>LEGALS</h2>
+                {/* child div */}
+                <div className="flex flex-col text-white    cursor-pointer">
+                  <span className=" opacity-50 hover:opacity-100">
+                    Terms of Service
+                  </span>
+                  <span className=" opacity-50 hover:opacity-100">
+                    Privacy Policy
+                  </span>
+                </div>
+              </div>
+
+              {/* inner child 2 */}
+              <div>
+                <h2>DISCLAIMER</h2>
+                {/* child div */}
+                <p className=" w-[300px]  text-white  opacity-50 ">
+                  Neither ShapeShift AG nor CoinCap are in any way associated
+                  with CoinMarketCap, LLC or any of its goods and services.
+                </p>
               </div>
             </div>
-
-            {/* inner child 2 */}
-            <div>
-              <h2>DISCLAIMER</h2>
-              {/* child div */}
-              <p className=" w-[300px]  text-white  opacity-50 ">
-                Neither ShapeShift AG nor CoinCap are in  any way associated with
-                CoinMarketCap,  LLC or any of its goods and services.
-              </p>
+          </div>
+          {/* social media */}
+          <div>
+            <div className="flex flex-col">
+              <h2>FOLLOWS US</h2>
+              <div className=" flex gap-2  cursor-pointer  ">
+                <span>
+                  {" "}
+                  <BsTwitter
+                    size={25}
+                    color="white"
+                    className=" opacity-50 hover:opacity-100 "
+                  />{" "}
+                </span>
+                <span>
+                  {" "}
+                  <AiFillFacebook
+                    size={25}
+                    color="white"
+                    className=" opacity-50 hover:opacity-100 "
+                  />{" "}
+                </span>
+              </div>
             </div>
           </div>
-        </div>
-        {/* social media */}
-        <div>
-          <div className="flex flex-col">
-            <h2>FOLLOWS US</h2>
-             <div className=" flex gap-2  cursor-pointer  ">        
-            <span> <BsTwitter size={25} color="white"  className=" opacity-50 hover:opacity-100 "  /> </span>
-            <span> <AiFillFacebook  size={25} color="white"  className=" opacity-50 hover:opacity-100 " /> </span>
-            </div>
-          </div>
-        </div>
         </div>
         {/* apllication */}
         <div>
@@ -64,21 +84,21 @@ const Footer = () => {
             <h2>COINCAP APP AVAILABLE ON</h2>
 
             {/* inner child */}
-         
-             <div className="flex flex-col gap-2">            
-            <a href="/"  className=" opacity-50 hover:opacity-100 ">
-              <img
-                src="	https://coincap.io/static/images/stores/google_play.svg"
-                alt="icon"
-              />
-            </a>
 
-            <a href="/"  className=" opacity-50 hover:opacity-100 ">
-              <img
-                src="	https://coincap.io/static/images/stores/apple_store.svg"
-                alt="icon"
-              />
-            </a>
+            <div className="flex flex-col gap-2">
+              <a href="/" className=" opacity-50 hover:opacity-100 ">
+                <img
+                  src="	https://coincap.io/static/images/stores/google_play.svg"
+                  alt="icon"
+                />
+              </a>
+
+              <a href="/" className=" opacity-50 hover:opacity-100 ">
+                <img
+                  src="	https://coincap.io/static/images/stores/apple_store.svg"
+                  alt="icon"
+                />
+              </a>
             </div>
           </div>
         </div>
